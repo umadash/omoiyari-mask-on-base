ecd4ab1dab231890e79a1a8494bba67eba108544
Yamato Hashimoto <yamato@umadash.com>
2020-05-13 17:05:39 +0900

    udpate

--- _src/ts/project/EntryIndex.ts
+++ _src/ts/project/EntryIndex.ts
@@ -1,10 +1,19 @@
 const $ = jQuery;
 const _ = require("lodash");
 
+import Slideshow from "../project/views/Slideshow";
+
 export class EntryIndex {
+  private slideshows: Slideshow[] = [];
   constructor() {}
 
   public run(): void {
-    alert("run");
+    const $slideshows = $(".Slideshow");
+    if ($slideshows.length > 0) {
+      $slideshows.each((index: number, element: HTMLElement) => {
+        const s: Slideshow = new Slideshow($(element));
+        this.slideshows.push(s);
+      });
+    }
   }
 }
