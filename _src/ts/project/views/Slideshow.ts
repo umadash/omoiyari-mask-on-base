7fb30720219839ea5b95c5a062913733884d3dc6
Yamato Hashimoto <yamato@umadash.com>
2020-05-13 17:31:00 +0900

    update

--- _src/ts/project/views/Slideshow.ts
+++ _src/ts/project/views/Slideshow.ts
@@ -2,6 +2,7 @@ export default class Slideshow {
   private $elm: JQuery;
   private $content: JQuery;
   private $thumbs: JQuery;
+  private srcs: string[];
 
   constructor($elm: JQuery) {
     this.$elm = $elm;
@@ -21,6 +22,24 @@ export default class Slideshow {
       .filter((value: string) => {
         return !!value;
       });
+    this.srcs = srcs;
     console.log(srcs);
+
+    // 画像の数だけ、サムネイルを生成する
+    let thumbs = "";
+    for (let i = 0; i < this.srcs.length; i++) {
+      const src: string = this.srcs[i];
+      thumbs += `<button data-index="${i}" class="BtnThumb" style="background-image:url(${src})"></button>`;
+    }
+    this.$thumbs.html(thumbs);
+
+    $(".BtnThumb").on("click", (e) => {
+      const $target = $(e.target);
+      const index = parseInt($target.attr("data-index"), 10);
+      const src = this.srcs[index];
+      this.$content.css({
+        backgroundImage: `url(${src})`,
+      });
+    });
   }
 }
